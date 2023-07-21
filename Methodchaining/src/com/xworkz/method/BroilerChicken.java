81d5d0ce8d742e6e9e0d097e4c4161ed92dbe8dd
Priya-Shivgonda <priyanka96.xworkz@gmail.com>
2023-07-21 10:09:59 +0530

    method-chaining

---
+++ Methodchaining/src/com/xworkz/method/BroilerChicken.java
@@ -0,0 +1,26 @@
+package com.xworkz.method;
+	
+	public class BroilerChicken extends Chicken{
+
+		boolean alive;
+		double weight;
+
+		public BroilerChicken(double price, String location, boolean alive, double weight) {
+			super(price, location);
+			this.alive = alive;
+			this.weight = weight;
+		}
+
+		@Override
+		void printInfo() {
+			System.out.println("Running print Info of Broiler Chicken");
+			super.printInfo();
+			System.out.println("Chicken alive status : " + alive);
+			System.out.println("Chicken weight : " + weight);
+
+		}
+
+
+	}
+
+
