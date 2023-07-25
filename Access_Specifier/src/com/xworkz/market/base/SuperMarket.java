1d370b4a0e62368fbfc696017b4d6d2415d7255c
Priya-Shivgonda <priyanka96.xworkz@gmail.com>
2023-07-25 11:37:03 +0530

    access specifier

---
+++ Access_Specifier/src/com/xworkz/market/base/SuperMarket.java
@@ -0,0 +1,16 @@
+package com.xworkz.market.base;
+
+public class SuperMarket  extends Market {
+
+	
+
+		@Override
+		public void sell() {
+
+			System.out.println("Running sell in Super Market");
+
+		}
+
+
+	}
+
---
+++ Access_Specifier/src/com/xworkz/market/online/OnlineMarket.java
@@ -0,0 +1,16 @@
+package com.xworkz.market.online;
+
+
+
+	import com.xworkz.market.base.Market;
+
+	public class OnlineMarket extends Market{
+
+		@Override
+		public void buy() {
+			System.out.println("Running buy in Online market");
+			}
+
+
+	}
+
